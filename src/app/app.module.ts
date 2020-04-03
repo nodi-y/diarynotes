3bd2eb1fccce4feab9c9f64228326b15ceefb3bf
Faiz Yunus <t7z9a@ugrad.cs.ubc.ca>
2020-04-02 19:15:15 -0700

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EmotionsComponent } from './components/emotions/emotions.component';
+import { UrgesComponent } from './components/urges/urges.component';
+import { ActionsComponent } from './components/actions/actions.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EmotionsComponent,
+    UrgesComponent,
+    ActionsComponent,
+    SkillsComponent,
+    DashboardComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
